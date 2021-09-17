2c87ab96940f60bcbe9ea0cd19f41954c6fd609f
Садрутдинов Айдар <a.i.sadrutdinov@kalashnikovconcern.ru>
2021-09-17 10:25:35 +0400

    7. Type erasure

--- src/com/sai/generics/ParameterizedClass.java
+++ src/com/sai/generics/ParameterizedClass.java
@@ -2,14 +2,24 @@ package com.sai.generics;
 
 public class ParameterizedClass {
     public static void main(String[] args) {
-        Info<String> info1 = new Info<>("hello");
-        System.out.println(info1);
-        String s = info1.getValue();
+//        Info<String> info1 = new Info<>("hello");
+//        System.out.println(info1);
+//        String s = info1.getValue();
+//
+//        Info<Integer> info2 = new Info<>(16);
+//        System.out.println(info2);
+//        Integer i = info2.getValue();
 
-        Info<Integer> info2 = new Info<>(16);
-        System.out.println(info2);
-        Integer i = info2.getValue();
+
     }
+//ERROR
+//    public void abc(Info<String> info) {
+//        String s = info.getValue();
+//    }
+//
+//    public void abc(Info<Integer> info) {
+//        Integer s = info.getValue();
+//    }
 }
 
 class Info<T> {
@@ -31,3 +41,17 @@ class Info<T> {
 
 
 }
+
+class Parent{
+    public void abc(Info<String> info) {
+        String s = info.getValue();
+    }
+}
+//ПРИ ИПСОЛЬЗОВАНИИ ДЖЕНЕРИКОВ ДЛЯ ДЖАВА МАШИНЫ ПРОИСХОДИТ СТИРАНИЕ ТИПОВ
+//  public void abc(Info<Integer> info) -> public void abc(Info info)
+
+//class Child extends Parent{
+//    public void abc(Info<Integer> info) {
+//        String s = info.getValue();
+//    }
+//}
