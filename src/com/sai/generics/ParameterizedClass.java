ee980f5042e041b5df1b5a223c0279d9d82d160f
Садрутдинов Айдар <a.i.sadrutdinov@kalashnikovconcern.ru>
2021-09-17 10:43:04 +0400

    8. Subtyping

--- src/com/sai/generics/ParameterizedClass.java
+++ src/com/sai/generics/ParameterizedClass.java
@@ -5,24 +5,21 @@ public class ParameterizedClass {
 //        Info<String> info1 = new Info<>("hello");
 //        System.out.println(info1);
 //        String s = info1.getValue();
-//
-//        Info<Integer> info2 = new Info<>(16);
-//        System.out.println(info2);
-//        Integer i = info2.getValue();
+
+        Info<Integer> info2 = new Info<>(16);
+        System.out.println(info2);
+        Integer i = info2.getValue();
+
+        Info<Double> info4 = new Info<>(16.21);
+        System.out.println(info4);
+        Double d = info4.getValue();
 
 
     }
-//ERROR
-//    public void abc(Info<String> info) {
-//        String s = info.getValue();
-//    }
-//
-//    public void abc(Info<Integer> info) {
-//        Integer s = info.getValue();
-//    }
+
 }
 
-class Info<T> {
+class Info<T extends Number> {
 
 
     private T value;
@@ -38,15 +35,13 @@ class Info<T> {
     public T getValue() {
         return value;
     }
-
-
 }
 
-class Parent{
-    public void abc(Info<String> info) {
-        String s = info.getValue();
-    }
-}
+//class Parent{
+//    public void abc(Info<String> info) {
+//        String s = info.getValue();
+//    }
+//}
 //ПРИ ИПСОЛЬЗОВАНИИ ДЖЕНЕРИКОВ ДЛЯ ДЖАВА МАШИНЫ ПРОИСХОДИТ СТИРАНИЕ ТИПОВ
 //  public void abc(Info<Integer> info) -> public void abc(Info info)
 
@@ -55,3 +50,11 @@ class Parent{
 //        String s = info.getValue();
 //    }
 //}
+
+interface I1 {
+
+}
+
+interface I2 {
+
+}
---
+++ src/com/sai/generics/Test2.java
@@ -0,0 +1,25 @@
+package com.sai.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args) {
+//        X x = new Y();
+//
+//        List<X> list = new ArrayList<>();
+
+
+//        List<Number> list = new ArrayList<Integer>();
+//        list.add(18);
+//        list.add(3.14);
+    }
+}
+
+class X {
+
+}
+
+class Y extends X{
+
+}
