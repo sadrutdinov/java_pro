36c2fb1fd22ddfd94505b5bc00d8ae79b613f357
Садрутдинов Айдар <a.i.sadrutdinov@kalashnikovconcern.ru>
2021-09-21 13:44:41 +0400

    20. Big O notation

---
+++ src/com/sai/collections/ArraysTest.java
@@ -0,0 +1,12 @@
+package com.sai.collections;
+
+import java.util.Arrays;
+
+public class ArraysTest {
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3, 4};
+        String[] strings = {"", "saa", "dasdasa"};
+
+        System.out.println(Arrays.toString(strings));
+    }
+}
