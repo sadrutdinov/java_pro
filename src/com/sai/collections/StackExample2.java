93e359390abe2df4998cf494bd80ddf4b76f80b7
Садрутдинов Айдар <a.i.sadrutdinov@kalashnikovconcern.ru>
2021-09-21 16:39:45 +0400

    22. Stack

---
+++ src/com/sai/collections/StackExample2.java
@@ -0,0 +1,23 @@
+package com.sai.collections;
+
+import java.util.Stack;
+
+public class StackExample2 {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("Zaur");
+        stack.push("Misha");
+        stack.push("Oleg");
+        stack.push("Katya");
+
+
+//        while (!stack.isEmpty()) {
+//            System.out.println(stack.pop());
+//            System.out.println(stack);
+//        }
+
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        System.out.println(stack);
+    }
+}
