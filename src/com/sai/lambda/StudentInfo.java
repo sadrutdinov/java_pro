cfa6386cec7c370c93dd021d28083ab76cdebe85
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2021-11-02 16:40:02 +0400

    46. Function

--- src/com/sai/lambda/StudentInfo.java
+++ src/com/sai/lambda/StudentInfo.java
@@ -1,6 +1,8 @@
 package com.sai.lambda;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class StudentInfo {
@@ -21,7 +23,7 @@ class Test {
         Student st2 = new Student("Nickolay", 'M', 28, 2, 6.4);
         Student st3 = new Student("Elena", 'F', 19, 1, 8.9);
         Student st4 = new Student("Petr", 'M', 35, 4, 7);
-        Student st5 = new Student("Mariya", 'F', 23, 3, 9.1);
+        Student st5 = new Student("Mariya", 'F', 23, 3, 7.4);
 
         ArrayList<Student> students = new ArrayList<>();
         students.add(st1);
@@ -32,27 +34,42 @@ class Test {
 
         StudentInfo info = new StudentInfo();
 
+        Function<Student, Double> f = student -> student.avgGrade;
 
-        Predicate<Student> p1 = s -> s.age < 30;
-        Predicate<Student> p2 = s -> s.avgGrade > 7.5;
-        Predicate<Student> p3 = s -> s.sex == 'M';
-        Predicate<Student> p4 = p1.and(p2).and(p3);
-        Predicate<Student> p5 = p1.or(p3);
-        Predicate<Student> p6 = p1.negate();
+        System.out.println(avgOfSomething(students, student -> (double) student.age));
 
 
-        info.testStudents(students, p1);
-        System.out.println("-----------------");
-        info.testStudents(students, p2);
-        System.out.println("-----------------");
-        info.testStudents(students, p3);
-        System.out.println("-----------------");
-        info.testStudents(students, p4);
-        System.out.println("-----------------");
-        info.testStudents(students, p5);
-        System.out.println("-----------------");
-        info.testStudents(students, p6);
+//        Predicate<Student> p1 = s -> s.age < 30;
+//        Predicate<Student> p2 = s -> s.avgGrade > 7.5;
+//        Predicate<Student> p3 = s -> s.sex == 'M';
+//        Predicate<Student> p4 = p1.and(p2).and(p3);
+//        Predicate<Student> p5 = p1.or(p3);
+//        Predicate<Student> p6 = p1.negate();
+//
+//
+//        info.testStudents(students, p1);
+//        System.out.println("-----------------");
+//        info.testStudents(students, p2);
+//        System.out.println("-----------------");
+//        info.testStudents(students, p3);
+//        System.out.println("-----------------");
+//        info.testStudents(students, p4);
+//        System.out.println("-----------------");
+//        info.testStudents(students, p5);
+//        System.out.println("-----------------");
+//        info.testStudents(students, p6);
 
 
+    }
+
+    private static double avgOfSomething(List<Student> list, Function<Student, Double> f) {
+        double result = 0;
+
+        for (Student student: list
+             ) {
+            result += f.apply(student);
+        }
+
+        return result/list.size();
     }
 }
