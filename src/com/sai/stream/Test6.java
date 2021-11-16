7bc911c6745e98f4282824a79da68c5cca3e226e
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2021-11-16 15:36:46 +0400

    55. Метод count

--- src/com/sai/stream/Test6.java
+++ src/com/sai/stream/Test6.java
@@ -16,7 +16,9 @@ public class Test6 {
         Stream<Integer> stream5 = Stream.of(1,2,3,4,5,1,2,3);
 
 
-        stream5.distinct().forEach(System.out::println);
+//        stream5.distinct().forEach(System.out::println);
+
+        System.out.println(stream5.count());
 
 
     }
