5f0b20ccaea8f17bbf32b707c0b15b935c786cda
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2021-11-17 11:21:39 +0400

    58. Метод collect: grouping и partitioning

---
+++ src/com/sai/stream/Test8.java
@@ -0,0 +1,47 @@
+package com.sai.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Test8 {
+    public static void main(String[] args) {
+        Student st1 = new Student("Ivan", 'M', 22, 3, 8.3);
+        Student st2 = new Student("Nickolay", 'M', 28, 2, 6.4);
+        Student st3 = new Student("Elena", 'F', 19, 1, 8.9);
+        Student st4 = new Student("Petr", 'M', 35, 4, 7);
+        Student st5 = new Student("Mariya", 'F', 23, 3, 7.4);
+
+        List<Student> students = new ArrayList<>();
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+        students.add(st4);
+        students.add(st5);
+
+//        Map<Integer, List<Student>> map =
+//                students.stream()
+//                .map(el -> {
+//                    el.setName(el.getName().toUpperCase());
+//                    return el;
+//                })
+//                .collect(Collectors.groupingBy(el -> el.getCourse()));
+//
+//
+//        for (Map.Entry<Integer, List<Student>> entry  : map.entrySet()
+//             ) {
+//            System.out.println(entry.getKey() + ": "+ entry.getValue());
+//        }
+
+        Map<Boolean, List<Student>> map = students.stream()
+                .collect(Collectors.partitioningBy(el -> el.getAvgGrade() > 7.5));
+
+
+        for (Map.Entry<Boolean, List<Student>> entry : map.entrySet()
+        ) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+    }
+}
