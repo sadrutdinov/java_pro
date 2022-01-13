8c15f7c87b10924463f64a7111d30007e362480b
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2022-01-13 16:28:17 +0400

    66. Варианты создания потоков

---
+++ src/com/sai/multithreading/Ex4.java
@@ -0,0 +1,14 @@
+package com.sai.multithreading;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("privet");
+            }
+        }).start();
+
+        new Thread(() -> System.out.println("hello")).start();
+    }
+}
