f37aeeb832e6d3e216733e7b089f620ce3c0dad0
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2022-01-13 15:26:49 +0400

    65. Ввведение в многопоточность

---
+++ src/com/sai/multithreading/Example1.java
@@ -0,0 +1,15 @@
+package com.sai.multithreading;
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("privet");
+
+
+
+        for (int i = 1; i < 10 ; i++) {
+            System.out.println(i);
+        }
+        System.out.println("poka");
+
+    }
+}
