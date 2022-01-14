f010f3b094ee8ec62f50a936f27411e48940bf59
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2022-01-14 15:04:26 +0400

    69. Понятия concurrency и parallelism. Понятия synchronous и asynchronous

--- src/com/sai/multithreading/Ex8.java
+++ src/com/sai/multithreading/Ex8.java
@@ -5,7 +5,7 @@ public class Ex8 extends Thread {
     public void run() {
         for (int i = 1; i < 10; i++) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -36,7 +36,7 @@ class MyRunnable1 implements Runnable {
     public void run() {
         for (int i = 1; i < 10; i++) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
