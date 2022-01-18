6d98bed39b43aa49a0962579ac817dec54ab2168
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2022-01-18 09:30:11 +0400

    75. Lock и ReentrantLock

---
+++ src/com/sai/multithreading/Bankomat.java
@@ -0,0 +1,52 @@
+package com.sai.multithreading;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bankomat {
+    public static void main(String[] args) throws InterruptedException {
+        Lock lock = new ReentrantLock();
+        new Employee("Zaur", lock);
+        new Employee("Oleg", lock);
+        new Employee("Elena", lock);
+
+        Thread.sleep(5000);
+        new Employee("Victor", lock);
+        new Employee("Marina", lock);
+
+    }
+}
+
+
+class Employee extends Thread {
+    String name;
+    private Lock lock;
+
+    public Employee(String name, Lock lock) {
+        this.name = name;
+        this.lock = lock;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        if (lock.tryLock()) {
+
+
+            try {
+
+//            lock.lock();
+                System.out.println(name + " пользуется банкоматом");
+                Thread.sleep(2000);
+                System.out.println(name + " завершил свои дела");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            System.out.println(name + " не хочет ждать в очереди");
+        }
+    }
+
+}
