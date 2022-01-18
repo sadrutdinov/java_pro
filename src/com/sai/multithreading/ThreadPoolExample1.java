ec7e54e8819c92733d5bf66085ad125656c0d7c1
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2022-01-18 11:49:03 +0400

    78. ThreadPool и ExecutorService. Часть 1

---
+++ src/com/sai/multithreading/ThreadPoolExample1.java
@@ -0,0 +1,41 @@
+package com.sai.multithreading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExample1 {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new RunnableImpl100());
+
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+
+        System.out.println("Main ends");
+
+    }
+}
+
+
+class RunnableImpl100 implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " begins work");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " ends work");
+
+
+    }
+}
+
+
