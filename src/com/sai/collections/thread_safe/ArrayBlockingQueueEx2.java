513f746137de9d3ecd8a4181b12218027a98ea5f
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2022-01-19 16:44:52 +0400

    89. ArrayBlockingQueue

---
+++ src/com/sai/collections/thread_safe/ArrayBlockingQueueEx2.java
@@ -0,0 +1,38 @@
+package com.sai.collections.thread_safe;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ArrayBlockingQueueEx2 {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue<>(4);
+
+        //Producer
+        new Thread(() -> {
+            int i = 0;
+            while (true) {
+                try {
+                    arrayBlockingQueue.put(++i);
+                    System.out.println("Producer добавил элемент " + i);
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        //Consumer
+        new Thread(() -> {
+            int i = 0;
+            while (true) {
+                try {
+                    Integer j = arrayBlockingQueue.take();
+                    System.out.println("Consumer забрал элемент " + j);
+                    Thread.sleep(9000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                i++;
+            }
+        }).start();
+    }
+}
