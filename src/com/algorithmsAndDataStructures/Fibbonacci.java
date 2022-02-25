41cb1ca2d7b96606f5204c8af623a1389bc5cf62
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2022-02-25 16:32:06 +0400

    Урок 3: Big O

--- src/com/algorithmsAndDataStructures/Fibbonacci.java
+++ src/com/algorithmsAndDataStructures/Fibbonacci.java
@@ -20,6 +20,7 @@ public class Fibbonacci {
     }
 
     // наивный медленный очевидный
+    // O(2^n) - первая версия
     private static long fibNaive(int n, long[] mem) {
 
         if (mem[n] != -1) {
@@ -33,7 +34,7 @@ public class Fibbonacci {
 
         return result;
     }
-
+    // O(n)
     private static long fibMain(int n) {
         if (n <= 1) return n;
 
@@ -50,16 +51,29 @@ public class Fibbonacci {
         return firstValue;
     }
 
+
+    // O (n)
     private static long fibEffective(int n) {
+
+        // O(n)
         long arr[] = new long[n + 1];
 
-        arr[0] = 0;
-        arr[1] = 1;
+        arr[0] = 0; // O(1)
+        arr[1] = 1; // O(1)
+
+        // n = 2 => итераций цикла 1
+        // n = 3 => итераций цикла 2
+        // n = 4 => итераций цикла 3
+
+        // O (n+n) => O(2n) => O(n)
 
         for (int i = 2; i <= n; i++) {
             arr[i] = arr[i - 1] + arr[i - 2];
         }
 
-        return arr[n];
+
+        return arr[n]; // O(1)
     }
 }
+
+// O(n) + O(1) + O(1) + O(n) + O(1) = O(2n) + O(3) = O(2n+3) = O(n)
